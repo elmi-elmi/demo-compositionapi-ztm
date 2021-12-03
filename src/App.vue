59a8da28a6de14fc6ce4befe2ec72a89557698e9
shahrokh <shahrokhelmi@gmail.com>
2021-12-03 17:10:42 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{ num }}</p>
+    <p>{{ double }}</p>
+    <button @click.prevent="increment">add one</button>
+    <p>{{ name + ' ' + lastName }}</p>
+    <input type="text" v-model="phrase">
+    <p>{{ inversePhrase }}</p>
+    <Alert :user="user"/>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref, reactive, toRefs, watch, computed, onBeforeMount, onMounted,} from 'vue';
+import Alert from '@/components/Alert';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Alert},
+  setup() {
+    onBeforeMount(() => {
+      console.log('on before mount');
+    });
+    onMounted(() => {
+      console.log('on mounted')
+    })
+    let num = ref(0);
+
+    function increment() {
+      num.value++
+    }
+
+    const user = reactive({
+      name: 'shahrokh',
+      lastName: 'is'
+    });
+
+    const double = computed(() => {
+      return num.value ** 2;
+    });
+
+    setTimeout(() => {
+      user.lastName = 'is not'
+    }, 1000);
+
+    const phrase = ref("");
+    const inversePhrase = ref("");
+
+    watch(() => {
+      inversePhrase.value = phrase.value.split("").reverse().join("");
+    })
+
+
+    return {num, increment, ...toRefs(user), phrase, inversePhrase, double, user};
   }
 }
 </script>
---
+++ src/components/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+<div v-show="flag" class="flag">
+  Name changed
+</div>
+</template>
+
+<script>
+import {computed} from "vue";
+
+export default {
+  name: "Alert",
+  props:['user'],
+  setup(props){
+
+    const flag = computed(()=>{
+      return props.user.lastName === 'is not';
+    })
+
+    return{flag};
+  }
+}
+</script>
+
+<style scoped>
+
+.flag{
+  color: red;
+}
+
+</style>
